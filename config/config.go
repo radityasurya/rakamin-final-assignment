5c585f5fee185df203a9e28ed32b957e7b7c5e1f
radityasurya <radityasurya@hotmail.com>
2023-03-05 23:50:40 +0100

    Add config

---
+++ config/config.go
@@ -0,0 +1,29 @@
+package config
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	DBHost         string `mapstructure:"DB_HOST"`
+	DBUserName     string `mapstructure:"DB_USER"`
+	DBUserPassword string `mapstructure:"DB_PASSWORD"`
+	DBName         string `mapstructure:"DB_NAME"`
+	DBPort         string `mapstructure:"DB_PORT"`
+
+	ServerPort string `mapstructure:"PORT"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigType("env")
+	viper.SetConfigName("app")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
